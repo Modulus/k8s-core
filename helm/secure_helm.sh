2ea362a3b63db25d0b743c5c3651eb9ebeb82f5e
John Sigvald Skauge <johnsigvald.skauge@tv2.no>
2019-08-28 09:19:33 +0200

    UPgradded, added debug pod config

--- helm/secure_helm.sh
+++ helm/secure_helm.sh
@@ -1,2 +1,2 @@
 kubectl patch deployment tiller-deploy --namespace=kube-system --type=json --patch='[{"op": "add", "path": "/spec/template/spec/containers/0/command", "value": ["/tiller", "--listen=localhost:44134"]}]'
-
+kubectl delete service tiller-deploy
